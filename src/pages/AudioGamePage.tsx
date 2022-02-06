0ec41efac240109ae04df861742cbd9032554f82
Katerina Vinskovska <kostiukkaterina@gmail.com>
2022-02-06 16:10:43 +0200

    feat: add project structure

---
+++ src/pages/AudioGamePage.tsx
@@ -0,0 +1,11 @@
+import '../styles/AudioGamePage.css';
+
+import { FC } from 'react';
+
+const AudioGamePage: FC = () => (
+  <div>
+    AudioGamePage
+  </div>
+);
+
+export default AudioGamePage;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import '../styles/MainPage.css';
+
+import { FC } from 'react';
+
+const MainPage: FC = () => (
+  <div>
+    MainPage
+  </div>
+);
+
+export default MainPage;
