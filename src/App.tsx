09496c40fc0f71f5c2a6dfa3cc7ceffbfd5d38c3
Katerina Vinskovska <kostiukkaterina@gmail.com>
2022-02-07 20:34:49 +0200

    fix: move routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import './styles/App.css';
-
 import { FC } from 'react';
-
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+import './styles/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import TextbookPage from './pages/TextbookPage';
+import AudioGamePage from './pages/AudioGamePage';
+import SprintGamePage from './pages/SprintGamePage';
+import StatisticsPage from './pages/StatisticsPage';
 
 const App: FC = () => (
-  <div className="App">
-    <Header />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/textbook" element={<TextbookPage />} />
+        <Route path="/audio" element={<AudioGamePage />} />
+        <Route path="/sprint" element={<SprintGamePage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,24 @@
-import '../styles/Header.css';
-
 import { FC } from 'react';
-import {
-  BrowserRouter, Link, Route, Routes,
-} from 'react-router-dom';
-
-import MainPage from '../pages/MainPage';
-import TextbookPage from '../pages/TextbookPage';
-import AudioGamePage from '../pages/AudioGamePage';
-import SprintGamePage from '../pages/SprintGamePage';
-import StatisticsPage from '../pages/StatisticsPage';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 
 const Header: FC = () => (
   <header className="header">
-    <BrowserRouter>
-      <div className="container">
-        <div className="header-wrapper">
-          <div>RS Lang</div>
-          <nav className="header-nav">
-            <Link className="header-link" to="/">Главная</Link>
-            <Link className="header-link" to="/textbook">Учебник</Link>
-            <div className="drop-down-menu">
-              <div>Игры</div>
-              <Link className="header-link" to="/audio">Аудиовызов</Link>
-              <Link className="header-link" to="/sprint">Спринт</Link>
-            </div>
-            <Link className="header-link" to="/statistics">Статистика</Link>
-          </nav>
-        </div>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/textbook" element={<TextbookPage />} />
-          <Route path="/audio" element={<AudioGamePage />} />
-          <Route path="/sprint" element={<SprintGamePage />} />
-          <Route path="/statistics" element={<StatisticsPage />} />
-        </Routes>
+    <div className="container">
+      <div className="header-wrapper">
+        <div>RS Lang</div>
+        <nav className="header-nav">
+          <Link className="header-link" to="/">Главная</Link>
+          <Link className="header-link" to="/textbook">Учебник</Link>
+          <div className="drop-down-menu">
+            <div>Игры</div>
+            <Link className="header-link" to="/audio">Аудиовызов</Link>
+            <Link className="header-link" to="/sprint">Спринт</Link>
+          </div>
+          <Link className="header-link" to="/statistics">Статистика</Link>
+        </nav>
       </div>
-    </BrowserRouter>
+    </div>
   </header>
 );
 
